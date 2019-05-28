607bfdf5d5bbccbddfef495234cb12f92d18d0bf
khaijiet <fantastasticbabii@gmail.com>
2019-05-28 21:08:21 +0800

    Add project files.

---
+++ JomMalaysia.Infrastructure/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace JomMalaysia.Infrastructure
+{
+    public class Class1
+    {
+    }
+}
---
+++ JomMalaysia.Test/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace JomMalaysia.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
