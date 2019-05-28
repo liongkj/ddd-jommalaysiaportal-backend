7bd500c39d681fb6461c67943a8aa118fef80b64
khaijiet <fantastasticbabii@gmail.com>
2019-05-28 23:17:21 +0800

    rename

--- JomMalaysia.Api/Program.cs
+++ JomMalaysia.Api/Program.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
-namespace JomMalaysia
+namespace JomMalaysia.Api
 {
     public class Program
     {
--- JomMalaysia.Api/Startup.cs
+++ JomMalaysia.Api/Startup.cs
@@ -11,7 +11,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 
-namespace JomMalaysia
+namespace JomMalaysia.Api
 {
     public class Startup
     {
