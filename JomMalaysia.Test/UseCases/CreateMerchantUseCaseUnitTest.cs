6c821729df3d67d3c6e1d36ef5e009e9c7f40558
liongkj <fantasticbabii@gmail.com>
2019-06-27 16:47:54 +0800

    merge

--- JomMalaysia.Test/UseCases/CreateMerchantUseCaseUnitTest.cs
+++ JomMalaysia.Test/UseCases/CreateMerchantUseCaseUnitTest.cs
@@ -9,23 +9,13 @@ namespace JomMalaysia.Test.UseCases
 {
     public class CreateMerchantUseCaseUnitTest
     {
+        JomMalaysia.Core.UseCases.MerchantUseCase.Create;
+using 
         [Fact]
         public void Can_Create_Merchant()
         {
             //store the user data somehow
-            var testMerchantRepository = new Mock<IMerchantRepository>();
-            testMerchantRepository
-                .Setup(repo => repo.CreateMerchant(It.IsAny<Merchant>()))
-                .Returns(new CreateMerchantResponse("", true));
 
-            //use case
-            var useCase = new CreateMerchantUseCase(testMerchantRepository.Object);
-            Address address = new Address("123", "s2a", "seremban", "seremban 2", "70300", "Malaysia");
-            Name name = new Name("kj", "Liong");
-            Email email = (Email)("khaijiet@hotmail.com");
-            Phone phone = (Phone)("018-636789");
-            var testOutputPort = new Mock<IOutputPort<CreateMerchantResponse>>();
-            testOutputPort.Setup(outputPort => outputPort.Handle(It.IsAny<CreateMerchantResponse>()));
 
 
             //var merchant = new CreateMerchantRequest();
