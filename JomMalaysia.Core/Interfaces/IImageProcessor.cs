f5f303fa44b94b2f5296458f4e148ffe14d5aca4
KJ <fantasticbabii@gmail.com>
2019-09-24 12:59:56 +0800

    fix missing package

--- JomMalaysia.Core/Interfaces/IImageProcessor.cs
+++ JomMalaysia.Core/Interfaces/IImageProcessor.cs
@@ -1,9 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using JomMalaysia.Core.Services.ImageProcessingServices;
-using Microsoft.AspNetCore.Http;
+
 
 namespace JomMalaysia.Core.Interfaces
 {
