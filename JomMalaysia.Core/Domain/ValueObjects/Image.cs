65c8613c89713fcf0b57a9172fa3434f4ce1d84e
KJ <fantasticbabii@gmail.com>
2019-10-21 17:46:56 +0800

    update default image

--- JomMalaysia.Core/Domain/ValueObjects/Image.cs
+++ JomMalaysia.Core/Domain/ValueObjects/Image.cs
@@ -5,7 +5,7 @@ namespace JomMalaysia.Core.Domain.ValueObjects
     public class Image : ValueObjectBase
     {
         public string Url { get; }
-        public string ThumbnailUrl { get; } = "https://cloudinary.com/documentation/upload_widget#upload_widget_options";
+        public string ThumbnailUrl { get; } = "https://res.cloudinary.com/jomn9-com/image/upload/v1571632729/category_thumbnail/proz1gzlepy9gp3rk0ej.jpg";
 
 
         Image(string url, string thumbnailurl)
