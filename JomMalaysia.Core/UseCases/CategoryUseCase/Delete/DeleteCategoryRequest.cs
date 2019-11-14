7f1c1dd2429c73e305088ecb48c81bcb207e2dd2
KJ <fantasticbabii@gmail.com>
2019-11-14 10:46:44 +0800

    fix delete category api

--- JomMalaysia.Core/UseCases/CategoryUseCase/Delete/DeleteCategoryRequest.cs
+++ JomMalaysia.Core/UseCases/CategoryUseCase/Delete/DeleteCategoryRequest.cs
@@ -7,17 +7,5 @@ namespace JomMalaysia.Core.UseCases.CatogoryUseCase.Delete
     public class DeleteCategoryRequest : IUseCaseRequest<DeleteCategoryResponse>
     {
         public string Id { get; set; }
-
-
-        public DeleteCategoryRequest(string Id)
-        {
-            if (string.IsNullOrWhiteSpace(Id))
-            {
-                throw new System.ArgumentException("Delete Category: Category name null", nameof(Id));
-            }
-
-
-            this.Id = Id;
-        }
     }
 }
