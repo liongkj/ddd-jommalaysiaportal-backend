57eb7c331531bcaea429d9274035476ac626f4e1
liongkj <fantasticbabii@gmail.com>
2019-12-30 12:22:35 +0800

    fix listing response

--- JomMalaysia.Core/UseCases/ListingUseCase/Get/ListingViewModel.cs
+++ JomMalaysia.Core/UseCases/ListingUseCase/Get/ListingViewModel.cs
@@ -26,7 +26,7 @@ namespace JomMalaysia.Core.UseCases.ListingUseCase.Get
 
         public class StoreTimesViewModel
         {
-            public int DayOfWeek { get; set; }
+            public string DayOfWeek { get; set; }
             public TimeSpan OpenTime { get; set; }
             public TimeSpan CloseTime { get; set; }
 
