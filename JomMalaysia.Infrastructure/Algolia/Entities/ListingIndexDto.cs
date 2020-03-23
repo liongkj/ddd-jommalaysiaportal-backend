10490f80bb9d0425d06633bc20279d7dd7c862a8
liongkj <fantasticbabii@gmail.com>
2020-03-23 18:45:38 +0800

    halfway restructure index data

--- JomMalaysia.Infrastructure/Algolia/Entities/ListingIndexDto.cs
+++ JomMalaysia.Infrastructure/Algolia/Entities/ListingIndexDto.cs
@@ -12,15 +12,36 @@ namespace JomMalaysia.Infrastructure.Algolia.Entities
         public string ObjectID { get; set; }
         public MerchantIndexDto Merchant { get; set; }
         public string ListingName { get; set; }
+        public string Photo { get; set; }
         public Description Description { get; set; }
-        public AddressViewModel Address { get; set; }
-        public List<ListingViewModel.StoreTimesViewModel> OperatingHours { get; set; }
+        public AddressIndexDto Address { get; set; }
 
         public ListingViewModel.CategoryPathViewModel Category { get; set; }
         public string CategoryType { get; set; }
 
         public ICollection<string> Tags { get; set; }
-        
+        public GeoIndexDto _geoloc { get; set; }
+    }
+    
+    public class AddressIndexDto
+    {
+        string Add1 { get; set; }
+
+        string Add2 { get; set; }
+    }
+
+    public class GeoIndexDto
+    {
+        public double lat { get; set; }
+
+        public double lng { get; set; }
+    }
+
+    public class CategoryIndexDto
+    {
+        //TODO
+        // "media_category": "tv series",                 // string attribute for filtering
+        // "subject_category": ["drugs", "divorced dad"], 
     }
 
     public class MerchantIndexDto
--- JomMalaysia.Infrastructure/Algolia/Mapping/AlgoliaDataProfile.cs
+++ JomMalaysia.Infrastructure/Algolia/Mapping/AlgoliaDataProfile.cs
@@ -1,6 +1,8 @@
 using AutoMapper;
+using JomMalaysia.Core.Domain.ValueObjects;
 using JomMalaysia.Core.UseCases.ListingUseCase.Get;
 using JomMalaysia.Core.UseCases.MerchantUseCase.Get.Response;
+using JomMalaysia.Core.UseCases.SharedRequest;
 using JomMalaysia.Infrastructure.Algolia.Entities;
 
 namespace JomMalaysia.Infrastructure.Algolia
@@ -12,10 +14,22 @@ namespace JomMalaysia.Infrastructure.Algolia
             CreateMap<ListingViewModel, ListingIndexDto>()
                 .ForPath(dto => dto.ObjectID, opt => opt.MapFrom(o => o.ListingId))
                 .ForPath(dto => dto.Merchant, opt => opt.MapFrom(o => o.Merchant))
+                .ForPath(dto=>dto.Photo,opt
+                    =>opt.MapFrom(o=>o.ListingImages.ListingLogo.Url))
+                .ForPath(dto=>dto._geoloc,opt=>opt.MapFrom(o=>o.Address.Coordinates))
                 ;
+
+            CreateMap<Coordinates, GeoIndexDto>()
+                .ForMember(dto => dto.lat, opt 
+                    => opt.MapFrom(o => o.Latitude))
+                .ForMember(dto => dto.lng, opt => opt.MapFrom(o => o.Longitude));
 
             CreateMap<ListingViewModel.MerchantVM, MerchantIndexDto>()
                 ;
+            
+            CreateMap<AddressViewModel, AddressIndexDto>();
+            
+           
         }
     }
 }
